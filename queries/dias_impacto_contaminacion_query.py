653a3d3ce27f4fd0bb1d1d192908e8eb2ffa4c04
unknown <guillermoperronelopez@gmail.com>
2025-04-25 09:29:11 -0300

    update query ejercicio 5

--- queries/dias_impacto_contaminacion_query.py
+++ queries/dias_impacto_contaminacion_query.py
@@ -1,7 +1,12 @@
 def dias_impacto_contaminacion_query(criteria):
     return """
-    SELECT vtp.fecha, ia.aqi, vtp.cantidad FROM produccion.indicador_calidad_aire ia
-    JOIN preproduccion.viajes_transporte_publico vtp ON vtp.fecha = ia.fecha
-    ORDER BY ia.aqi {}, vtp.cantidad {} 
-    LIMIT 10 
+    SELECT vtp.fecha, ica.aqi, vtp.cantidad 
+FROM produccion.indicador_calidad_aire ica 
+  JOIN (
+  SELECT fecha, SUM(cantidad) as cantidad 
+    FROM preproduccion.viajes_transporte_publico 
+    GROUP BY fecha
+    ) vtp ON vtp.fecha = ica.fecha 
+ORDER BY ica.aqi {}, vtp.cantidad {} 
+LIMIT 10  
     """.format(criteria,criteria)
